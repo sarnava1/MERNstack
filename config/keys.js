c1cbfea08d07e2f3490382e25ffcfc5a39c4401d
sarnava1 <konarsarnava@gmail.com>
2019-02-27 17:40:58 +0530

    connected the mlab db to our app

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://sarnava:iwonttellyou1@ds155045.mlab.com:55045/devconnector'
+}
--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-const express= require('express'); 
+const express = require('express'); 
+const mongoose = require('mongoose');
 
 const app=express();
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+//connect to mongoDB
+mongoose
+   .connect(db)
+   .then( () => console.log('MongoDB Connected'))
+   .catch(err => console.log(err));
+
 
 app.get('/', ( req, res ) => {
      res.send('Hello!!');
