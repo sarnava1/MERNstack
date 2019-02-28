cfbc31f48095d9d97704fa81a2967ef97b35cd74
sarnava1 <konarsarnava@gmail.com>
2019-03-01 03:13:16 +0530

    added the users registration part and tested by postman

--- models/User.js
+++ models/User.js
@@ -17,8 +17,8 @@ const UserSchema = new Schema({
         required: true
      },
      avatar: {
-        type: String,
-        required: true
+        type: String
+        
      },
      date: {
         type: Date,
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
+const User = require('../../models/User');
+
+//TEST ROUTE
 router.get('/test', (req, res) => res.json({ msg : 'users test'}));
+
+//REGISTER ROUTE
+router.post('/register', (req, res) => {
+      
+    User.findOne({ email: req.body.email }).then(user => {
+      if (user) {
+        errors.email = 'Email already exists';
+        return res.status(400).json(errors);
+      } else {
+        const avatar = gravatar.url(req.body.email, {
+          s: '200', // Size
+          r: 'pg', // Rating
+          d: 'mm' // Default
+        });
+  
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar,
+          password: req.body.password
+        });
+  
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          });
+        });
+      }
+    });
+  });
+  
 
 module.exports = router;
