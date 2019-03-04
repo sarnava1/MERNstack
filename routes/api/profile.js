0fa1c28fdfc2151506a66db81ddb273d76b9da0b
sarnava1 <konarsarnava@gmail.com>
2019-03-04 17:15:47 +0530

    added more routes for the profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -97,4 +97,56 @@ router.post(
     }
   );
 
+
+  router.get('/all', (req, res) => {
+    const errors = {};
+  
+    Profile.find()
+      .populate('user', ['name', 'avatar'])
+      .then(profiles => {
+        if (!profiles) {
+          errors.noprofile = 'There are no profiles';
+          return res.status(404).json(errors);
+        }
+  
+        res.json(profiles);
+      })
+      .catch(err => res.status(404).json({ profile: 'There are no profiles' }));
+  });
+  
+  
+  router.get('/handle/:handle', (req, res) => {
+    const errors = {};
+  
+    Profile.findOne({ handle: req.params.handle })
+      .populate('user', ['name', 'avatar'])
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = 'There is no profile for this user';
+          res.status(404).json(errors);
+        }
+  
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  });
+  
+
+  router.get('/user/:user_id', (req, res) => {
+    const errors = {};
+  
+    Profile.findOne({ user: req.params.user_id })
+      .populate('user', ['name', 'avatar'])
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = 'There is no profile for this user';
+          res.status(404).json(errors);
+        }
+  
+        res.json(profile);
+      })
+      .catch(err =>
+        res.status(404).json({ profile: 'There is no profile for this user' })
+      );
+  });
 module.exports = router;
