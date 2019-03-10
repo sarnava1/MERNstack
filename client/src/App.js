d0270060d07830e8ef4f3b45e5e4f92a9876a032
sarnava1 <konarsarnava@gmail.com>
2019-03-10 23:06:14 +0530

    added other auth components and installed react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,26 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 
 class App extends Component {
   render() {
     return (
+    <Router>
       <div className="App">
         <Navbar/>
+        <Route exact path="/" component={Landing} />
+        <div className="container">
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </div>
         <Landing />
         <Footer />
       </div>
+    </Router>  
     );
   }
 }
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Register extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Register</h1>
+      </div>
+    )
+  }
+
+}
+
+
+export default Register;
