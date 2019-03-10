9c3c36142db50c5444a63f7d7835c928d08da246
sarnava1 <konarsarnava@gmail.com>
2019-03-10 20:16:33 +0530

    added the initial components

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+  render() {
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            DevConnector
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#mobile-nav"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          <div className="collapse navbar-collapse" id="mobile-nav">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/profiles">
+                  {' '}
+                  Developers
+                </Link>
+              </li>
+            </ul>
+            {isAuthenticated ? authLinks : guestLinks}
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default Navbar;
