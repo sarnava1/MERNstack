34169c17a96bfb42ff12af98c33d6d178e9924fd
sarnava1 <konarsarnava@gmail.com>
2019-03-14 00:30:06 +0530

    added the boilerplate for redux

---
+++ client/src/store.js
@@ -0,0 +1,20 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+//first parameter is the root reducer, the second para is for the inital state 
+//and the 3rd parameter is for the middleware
+const store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )    
+);
+
+export default store;
--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb://sarnava:iwonttellyou1@ds155045.mlab.com:55045/devconnector',
-    secretOrKey: 'secret'
-}
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+  } else {
+    module.exports = require('./keys_dev');
+  }
