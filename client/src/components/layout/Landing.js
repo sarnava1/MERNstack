6e6ddf5392c4ce57a62ef9b7cde698ea65caa89d
sarnava1 <konarsarnava@gmail.com>
2019-03-17 01:52:10 +0530

    added Landing component

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,10 +1,18 @@
-import React, { Component } from 'react'
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
 
 class Landing extends Component {
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
+  }
+
   render() {
     return (
-        <div className="landing">
+      <div className="landing">
         <div className="dark-overlay landing-inner text-light">
           <div className="container">
             <div className="row">
@@ -27,8 +35,16 @@ class Landing extends Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Landing;
+Landing.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(Landing);
