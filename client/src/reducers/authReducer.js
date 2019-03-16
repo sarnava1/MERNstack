a1ddd32e7c40176f070c5556b933f20a5a248fad
sarnava1 <konarsarnava@gmail.com>
2019-03-17 01:36:16 +0530

    added authReducer reducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,11 +1,21 @@
-const initialState= {
-    isAuthenticated: false,
-    user: {}
-}
+import isEmpty from '../validation/is-empty';
+
+import { SET_CURRENT_USER } from '../actions/types';
+
+const initialState = {
+  isAuthenticated: false,
+  user: {}
+};
 
 export default function(state = initialState, action) {
-   switch(action.type){
-       default:
-         return state;
-   }
+  switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !isEmpty(action.payload),
+        user: action.payload
+      };
+    default:
+      return state;
+  }
 }
