d0e6c1bec319c2893bc047bdf0cd51675a0f8464
sarnava1 <konarsarnava@gmail.com>
2019-03-25 21:57:09 +0530

    added the edit-profile component

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-profile/CreateProfile';
+import EditProfile from './components/edit-profile/EditProfile';
 
 import './App.css';
 
@@ -56,7 +57,10 @@ class App extends Component {
               <Switch>
                  <PrivateRoute exact path="/create-profile" component={CreateProfile} />
               </Switch>
-            </div>
+              <Switch>
+              <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+            </Switch>
+          </div>
             <Footer />
           </div>
         </Router>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -18,6 +18,11 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
+          <Link className="nav-link" to="/dashboard">
+            Dashboard
+          </Link>
+        </li>
+        <li className="nav-item">
           <a
             href=""
             onClick={this.onLogoutClick.bind(this)}
@@ -55,7 +60,7 @@ class Navbar extends Component {
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
           <Link className="navbar-brand" to="/">
-            DevNet
+            DevConnector
           </Link>
           <button
             className="navbar-toggler"
