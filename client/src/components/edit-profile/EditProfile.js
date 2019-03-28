e8bf9b2bf0d2a43a6bc9b657a9f48f15485276ca
sarnava1 <konarsarnava@gmail.com>
2019-03-29 01:50:22 +0530

    Dashboard component to view and delete the education

--- client/src/components/edit-profile/EditProfile.js
+++ client/src/components/edit-profile/EditProfile.js
@@ -8,6 +8,7 @@ import InputGroup from '../common/InputGroup';
 import SelectListGroup from '../common/SelectListGroup';
 import { createProfile, getCurrentProfile } from '../../actions/profileActions';
 import isEmpty from '../../validation/is-empty';
+import { Link, withRouter } from 'react-router-dom';
 
  class CreateProfile extends Component {
   constructor(props) {
@@ -192,6 +193,9 @@ import isEmpty from '../../validation/is-empty';
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
+              <Link to="/dashboard" className="btn btn-light">
+                Go Back
+              </Link>
               <h1 className="display-4 text-center">Edit Profile</h1>
               <small className="d-block pb-3">* = required fields</small>
               <form onSubmit={this.onSubmit}>
