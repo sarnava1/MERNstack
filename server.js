6401886e0a2269759c696f3e7bf6cd70765c1998
sarnava1 <konarsarnava@gmail.com>
2019-05-19 00:05:59 +0530

    prepared for deploy

--- server.js
+++ server.js
@@ -34,6 +34,15 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
+//serve static assets if we are in production
+if(process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
