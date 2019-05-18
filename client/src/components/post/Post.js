5087356502c0f2784da6ae687b7bc227b64000d7
sarnava1 <konarsarnava@gmail.com>
2019-05-18 08:20:44 +0530

    added the form component for adding comments

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -3,29 +3,31 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import PostItem from '../posts/PostItem';
+import CommentForm from './CommentForm';
 import Spinner from '../common/Spinner';
 import { getPost } from '../../actions/postActions';
 
- class Post extends Component {
+class Post extends Component {
   componentDidMount() {
     this.props.getPost(this.props.match.params.id);
   }
 
-   render() {
+  render() {
     const { post, loading } = this.props.post;
     let postContent;
 
-     if (post === null || loading || Object.keys(post).length === 0) {
+    if (post === null || loading || Object.keys(post).length === 0) {
       postContent = <Spinner />;
     } else {
       postContent = (
         <div>
           <PostItem post={post} showActions={false} />
+          <CommentForm postId={post._id} />
         </div>
       );
     }
 
-     return (
+    return (
       <div className="post">
         <div className="container">
           <div className="row">
@@ -42,13 +44,13 @@ import { getPost } from '../../actions/postActions';
   }
 }
 
- Post.propTypes = {
+Post.propTypes = {
   getPost: PropTypes.func.isRequired,
   post: PropTypes.object.isRequired
 };
 
- const mapStateToProps = state => ({
+const mapStateToProps = state => ({
   post: state.post
 });
 
- export default connect(mapStateToProps, { getPost })(Post);
+export default connect(mapStateToProps, { getPost })(Post);
